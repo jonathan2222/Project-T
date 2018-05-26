a90d20e7536da752c362d5572add0a7faeaea6b0
Jonathan Åleskog <jonathan9609082000@gmail.com>
2018-05-26 17:51:22 +0200

    Testing Networking.

--- Project T/main.cpp
+++ Project T/main.cpp
@@ -1,27 +1,94 @@
 #include <iostream>
-#include "SFML\Graphics.hpp"
+#include "SFML\Network.hpp"
+
+const unsigned short PORT = 5000;
+const std::string IPADDRESS("192.168.10.163");//change to suit your needs
+
+std::string msgSend;
+
+sf::TcpSocket socket;
+sf::Mutex globalMutex;
+bool quit = false;
+
+void doStuff(void)
+{
+	static std::string oldMsg;
+	while (!quit)
+	{
+		sf::Packet packetSend;
+		globalMutex.lock();
+		packetSend << msgSend;
+		globalMutex.unlock();
+
+		socket.send(packetSend);
+
+		std::string msg;
+		sf::Packet packetReceive;
+
+		socket.receive(packetReceive);
+		if ((packetReceive >> msg) && oldMsg != msg && !msg.empty())
+		{
+			std::cout << msg << std::endl;
+			oldMsg = msg;
+		}
+	}
+}
+
+void server(void)
+{
+	sf::TcpListener listener;
+	listener.listen(PORT);
+	listener.accept(socket);
+	std::cout << "New client connected: " << socket.getRemoteAddress() << std::endl;
+}
+
+bool client(void)
+{
+	if (socket.connect(IPADDRESS, PORT) == sf::Socket::Done)
+	{
+		std::cout << "Connected\n";
+		return true;
+	}
+	return false;
+}
+
+void getInput(void)
+{
+	std::string s;
+	std::cout << "\nEnter \"exit\" to quit or message to send: ";
+	getline(std::cin, s);
+	if (s == "exit")
+		quit = true;
+	globalMutex.lock();
+	msgSend = s;
+	globalMutex.unlock();
+}
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(600, 600), "Project T");
+	sf::Thread* thread = nullptr;
 
-	sf::RectangleShape rect(sf::Vector2f(200, 200));
-	rect.setPosition(200, 200);
+	char who;
+	std::cout << "Client (c) or Server (s): ";
+	std::cin >> who;
 
-	while (window.isOpen())
+	if (who == 's')
+		server();
+	else 
+		client();
+
+	thread = new sf::Thread(&doStuff);
+	thread->launch();
+
+	while (!quit)
 	{
-		sf::Event evnt;
-		while (window.pollEvent(evnt))
-		{
-			if (evnt.type == sf::Event::Closed)
-			{
-				window.close();
-			}
-		}
+		getInput();
+	}
 
-		window.clear();
-		window.draw(rect);
-		window.display();
+	if (thread)
+	{
+		thread->wait();
+		delete thread;
 	}
 
 	return 0;
