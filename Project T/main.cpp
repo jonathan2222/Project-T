dc76d46df94c5fd7d3f4db9c8b51a7959a3a1b6b
Jonathan Åleskog <jonathan9609082000@gmail.com>
2018-05-26 16:55:02 +0200

    Added sfml.

--- Project T/main.cpp
+++ Project T/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include "SFML\Graphics.hpp"
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
-	std::cin.get();
+	sf::RenderWindow window(sf::VideoMode(600, 600), "Project T");
+
+	sf::RectangleShape rect(sf::Vector2f(200, 200));
+	rect.setPosition(200, 200);
+
+	while (window.isOpen())
+	{
+		sf::Event evnt;
+		while (window.pollEvent(evnt))
+		{
+			if (evnt.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear();
+		window.draw(rect);
+		window.display();
+	}
+
 	return 0;
 }
